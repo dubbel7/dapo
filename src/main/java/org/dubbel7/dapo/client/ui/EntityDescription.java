0b7f5cf6dd26cfc07a7fdfdd3b4e204bbc9b41d0
dubbel7 <vladimir.kvasov@gmail.com>
2015-12-11 13:58:40 +0000

    Create EntityDescription.java

---
+++ src/main/java/org/dubbel7/dapo/client/ui/EntityDescription.java
@@ -0,0 +1,28 @@
+package org.dubbel7.dapo.client.ui;
+
+public class EntityDescription {
+
+
+    private final String name;
+    private final String primaryKeyName;
+    private final String[] columns;
+
+
+    public EntityDescription(String name, String primaryKeyName, String[] columns) {
+        this.name = name;
+        this.primaryKeyName = primaryKeyName;
+        this.columns = columns;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrimaryKeyName() {
+        return primaryKeyName;
+    }
+
+    public String[] getColumns() {
+        return columns;
+    }
+}
